183681b50918c99d19959a61fef5dfb3899bb017
Potter <aa4790139@gmail.com>
2024-09-03 16:57:40 +0800

    feat: upgrage from sitemap to @nuxtjs/seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/sitemap'],
-
+  modules: ['@nuxtjs/seo'],
+  devtools: { enabled: true },
   nitro: {
     prerender: {
       crawlLinks: true,
